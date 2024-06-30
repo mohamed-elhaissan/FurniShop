69f9e3752a0bbeb87456be1fd84d7d3ddbad32c4
mohamed <mhalwasoffice@gmail.com>
2024-06-30 22:18:35 +0100

    updating

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const spanArray = document.querySelectorAll('.menu span');
-const menu = document.querySelector('.menu');
 
 
 
 
-var spanDone = true;
-menu.addEventListener('click',function(){
-    if(spanDone){
-        spanArray[0].classList.add('retate');
-        spanArray[2].classList.add('ReverseRetate');
-        spanArray[1].classList.add('none');
-        spanDone = false;
-    }
-    else {
-        spanArray[0].classList.remove('retate');
-        spanArray[2].classList.remove('ReverseRetate');
-        spanArray[1].classList.remove('none');
-        spanDone = true;
-    }
-    
-})
+
+
+
+
+
+
+document.querySelector('ion-icon').addEventListener('mouseenter',function(){
+    this.setAttribute('name','search');
+});
+document.querySelector('ion-icon').addEventListener('mouseleave',function(){this.setAttribute('name','search-outline');})
+
+
